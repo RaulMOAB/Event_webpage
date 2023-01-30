ea8f2c1a03d983c03d939edfbef3ac64a73aa369
RaulMOAB <raulmontoroabaijon@gmail.com>
2023-01-30 18:07:24 +0100

    Modify events v.2

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { UsersDbService } from 'src/app/services/users-db.service';
 
@@ -10,10 +11,11 @@ import { UsersDbService } from 'src/app/services/users-db.service';
 export class NavbarComponent implements OnInit{
 
   has_logged:boolean = false;
+  has_localStorage!:string;
   
 
 
-  constructor(private service:UsersDbService, private myCookie: CookieService){}
+  constructor(private service:UsersDbService, private myCookie: CookieService, private router:Router){}
   
   ngOnInit(): void {
     this.myCookie.get('user_cookie');
@@ -23,10 +25,13 @@ export class NavbarComponent implements OnInit{
               
       }          
     );
+    this.has_localStorage = localStorage.getItem('has_logged')!;
   }
 
   logout(){
+    this.has_localStorage = localStorage.removeItem('has_logged')!;
     this.myCookie.delete('user_cookie');
     this.has_logged = false;
+    this.router.navigate(['/login-form']);
   }
 }
